c9a6d674f476a9183de68c244118af8a7c40f4a5
suman98kumar <suman98kumar@gmail.com>
2022-12-01 02:00:47 -0500

    Developing api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,13 @@
-const si = require('systeminformation');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const si = require("systeminformation");
 
-si.wifiNetworks().then((data) => console.log(data));
+app.use(cors());
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
+
+app.get("/info", (_req, res) => {
+  si.wifiNetworks().then((data) => res.send(data));
+});
